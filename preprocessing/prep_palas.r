d201e901a3a7e09155908de75b4be882a52e1597
nb22i345 <nb22i345@staff-203-209.eduroam.unibe.ch>
2023-01-10 11:40:26 +0100

    concluding palas analysis and other minor changes

--- preprocessing/prep_palas.r
+++ preprocessing/prep_palas.r
@@ -61,12 +61,13 @@ for (i in 1:nrow(roomplan)) {
     mutate(time_min = hour(time) * 60 + minute(time)) %>%
     filter(school == roomplan$school[i]) %>%
     filter(weekday == roomplan$day[i]) %>%
-    # TODO: How many minutes before or after start of lecture? Currently: 0min 
-    # TODO: How many minutes before or after end of lecture? Currently: 0min
+    # How many minutes before or after start of lecture? Currently: 0min 
+    # How many minutes before or after end of lecture? Currently: 0min
     filter(between(time_min, roomplan$start_min[i], roomplan$end_min[i])) %>%
     mutate(class = ifelse(roomplan$school[i] == "School 1",paste0("Study (", roomplan$class[i], ")"), "Study"))  
 }
 df_filt <- do.call(rbind, filt_df_list) %>%
+  dplyr::filter(!no_school) %>%
   mutate(no_class = F) 
 
 df_full <- df %>% 
@@ -81,19 +82,199 @@ df_full <- df %>%
                         ifelse(class == "Study (B3d)", "Study (B)", class))) 
 
 
-ggplot(mapping = aes(x = time, y = co2ppm, color = factor(week))) +
-  geom_line(data = df_full %>% 
-              filter(!no_class) %>%
-              complete(school, week, weekday, time), mapping = aes(color = factor(week)), size = 1 / cm(1)) +
-  geom_line(data = df_full %>% 
-              filter(no_class) %>%
-              complete(school, week, weekday, time), mapping = aes(color = factor(week)), alpha = .25, size = 1 / cm(1)) +
-  facet_wrap(~school + weekday) 
 
+#### Outliers ####
+
+plot_curves <- function(var = "co2ppm") {
+  ggplot(mapping = aes(x = time, y = !! sym(var), color = factor(week))) +
+    geom_line(data = df_full %>% 
+                filter(!no_class) %>%
+                filter(!no_school) %>%
+                complete(school, week, weekday, time), mapping = aes(color = factor(week)), size = 1 / cm(1)) +
+    geom_line(data = df_full %>% 
+                filter(no_class) %>%
+                complete(school, week, weekday, time), mapping = aes(color = factor(week)), alpha = .25, size = 1 / cm(1)) +
+    facet_wrap(~school + weekday) 
+}
+
+# CO2
+
+#' CO2 in the outdoor air is at least 400 based on measurements in a nearby area:
+#' https://gml.noaa.gov/aftp/data/trace_gases/co2/flask/surface/txt/co2_hpb_surface-flask_1_ccgg_month.txt
+#' However, we encounter CO2 levels even below 400, so maybe near the schools the
+#' CO2 levels were actually lower, or the measurements are inaccurate below 400.
+
+df_full %>% filter(week == 6) %>% select(school, class, date, time, co2ppm) %>% ggplot(aes(x = time, color = school, y = co2ppm)) + facet_wrap(~date) + geom_point()
+
+#' The measurements during weeks without school are not consistent between schools but vary around 400 to 420, so in the range of outdoor levels.
+#' We therefore set a minimum of 400 and set all values below 400 to 400.
+
+co2ppm_out <- 400
+
+df_filt$co2ppm <- ifelse(df_filt$co2ppm < co2ppm_out, co2ppm_out, df_filt$co2ppm)
+df_full$co2ppm <- ifelse(df_full$co2ppm < co2ppm_out, co2ppm_out, df_full$co2ppm)
+
+df_full %>%
+  ggplot(aes(x = co2ppm, fill = school, color = school)) +
+  geom_density(alpha = .5)
+plot_curves()
+
+#' at the top the values are continuous --> probably no outliers --> keep
+
+
+# Temperature
+
+# hist(df_filt$tc)
+# plot_curves("tc")
+
+#' abnormal changes --> remove
+
+df_full <- df_full %>%
+  group_by(school) %>%
+  arrange(date, time) %>%
+  mutate(delta_tc = tc - dplyr::lag(tc)) %>%
+  ungroup() %>%
+  mutate(tc2 = ifelse(!is.na(delta_tc) & abs(delta_tc) > 2, NA, tc))
+
+plot_curves("tc2")
+
+df_filt <- df_filt %>%
+  left_join(df_full %>% select(school, date, time, tc2)) %>%
+  select(-tc) %>%
+  rename(tc = tc2)
+df_full <- df_full %>% 
+  select(-delta_tc, -tc) %>%
+  rename(tc = tc2)
+
+
+# Humidity
+
+hist(df_filt$rh)
+plot_curves("rh")
+
+#' some almost vertical lines, i.e. sudden changes --> remove
+
+df_full <- df_full %>%
+  group_by(school) %>%
+  arrange(date, time) %>%
+  mutate(delta_rh = rh - dplyr::lag(rh)) %>%
+  ungroup() %>%
+  mutate(rh2 = ifelse(!is.na(delta_rh) & abs(delta_rh) > 5, NA, rh))
+
+plot_curves("rh2")
+
+df_filt <- df_filt %>%
+  left_join(df_full %>% select(school, date, time, rh2)) %>%
+  select(-rh) %>%
+  rename(rh = rh2)
+df_full <- df_full %>% 
+  select(-delta_rh, -rh) %>%
+  rename(rh = rh2)
+
+
+# particle number concentration
+
+plot_curves("cn1cm")
+hist(df_filt$cn1cm)
+
+
+# particle matter concentration
+
+#' note that there are two extreme spikes in large particles of sizes 4 and 10 
+#' on Tuesday in week 5 and 7
+
+plot_curves("pm1mugm")
+hist(df_filt$pm1mugm)
+plot_curves("pm25mugm")
+hist(df_filt$pm25mugm)
+plot_curves("pm4mugm")
+hist(df_filt$pm4mugm)
+plot_curves("pm10mugm")
+hist(df_filt$pm10mugm)
+
+#### Compute additional variables ###
+
+# No. of students
+
+absences <- read.csv("data-clean/redcap-absences.csv")
+redcap_full <- readRDS("data-clean/redcap-full-range.rds")
+
+no_stud <- absences %>% 
+  left_join(redcap_full %>% 
+              select(school, class, no_school)) %>%
+  filter(!no_school) %>%
+  filter(class != "Control") %>%
+  mutate(n = n_class - n_absent + 1) %>%
+  group_by(school, date) %>%
+  summarize(n = mean(n)) %>%
+  ungroup() %>%
+  mutate(date = as.Date(date))
+
+df_filt <- df_filt %>%
+  left_join(no_stud, by = c("school", "date")) 
+
+#' Ventilation and air exchange rate
+#' Ventilation rate is measured in 1/h based on CO2 levels as
+#' Q = 3600 * G / (co2ppm - co2ppm_out),
+#' where:
+#' - G is the CO2 generation rate
+#' - co2ppm is the measured indoor CO2 level
+#' - co2ppm_out is the CO2 level in outdoor air
+#' G is taken from Persily et al. for age group 11 to 16, where it is ~0.004
+
+G <- 0.004
+
+hist(df_filt$co2ppm[df_filt$co2ppm<500])
+hist(df_filt$co2ppm)
+
+co2ppm_out <- round(min(df_filt$co2ppm, na.rm = T))
+print(co2ppm_out)
+
+#' The air exchange rate takes into account the number of people and volume of the room
+#' and is computed as
+#' AER = Q * n / V,
+#' where:
+#' - n is the number of people in the room and
+#' - V is the volume of the room in m3
+
+df_filt$V <- ifelse(df_filt$school == "School 1", 207, 233)
+
+#' Rebreathed air volume
+#' Measured in l/min per person as
+#' RAV = p * f0,
+#' where 
+#' - p is the respiratory volume  (8l per minute)
+#' - f0 is the rebreathed air fraction from other people
+#' The rebreathed air fraction is computed from CO2 levels as
+#' f = (co2ppm - co2ppm_out) / co2ppm_ex,
+#' where in addition to before:
+#' - co2ppm_ex is the Co2 concentration in exhaled air (ppm)
+#' The CO2 in exhaled air is taken from Emmerich et al.
+#' The rebreathed air fraction from other people then is
+#' f0 = f * (n - 1) / n,
+
+p <- 8
+co2ppm_ex <- 3900
+
+# Absolute humidity
+#' Supposed to be a better predictor of viral survival.
+#' https://royalsocietypublishing.org/doi/10.1098/rsif.2018.0298
+#' Use formula 2.1 and compute it based on temperature and relative humidity.
+
+df_filt <- df_filt %>%
+  mutate(f = ((co2ppm + 1) - co2ppm_out) / co2ppm_ex, # add 1 in case co2ppm is exactly equal to co2ppm_out 
+         f0 = f * (n - 1) / n,
+         rav = p * f0,
+         Q = 3600 * G / (((co2ppm+1) - co2ppm_out) / 1e3), # add 1 in case co2ppm is exactly equal to co2ppm_out
+         aer = Q * n / V,
+         ah = 1322.7 * exp(17.625 * tc / (tc + 243.04)) * (rh / 100) / (tc + 273.15)) %>%
+  select(school, date, week, weekday, time, intervention, airfilter, maskmandate, n, everything())
 
 #### Variable Subset ####
 
-df_full <- df_full %>%
-  select(school, class, date, week, weekday, time, no_school, no_class, maskmandate, airfilter, intervention, everything())
+df_filt <- df_filt %>%
+  select(school, class, date, week, weekday, time, intervention, maskmandate, airfilter, 
+         n, co2ppm, tc, rh, V, f, f0, rav, Q, aer, ah,
+         cn1cm, pm1mugm, pm25mugm, pm4mugm, pm10mugm)
 
-write.csv(df_full, "data-clean/palas.csv", row.names = F)
+write.csv(df_filt, "data-clean/palas.csv", row.names = F)
